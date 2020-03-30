d84660ec400756885f5c62d77c879f7af560e3a4
Hyunjin95 <das01063@snu.ac.kr>
2020-03-30 22:54:31 +0900

    Set up the project environment

---
+++ src/test/java/kr/ac/snu/ids/PRJ1_1_2013_11431/AppTest.java
@@ -0,0 +1,20 @@
+package kr.ac.snu.ids.PRJ1_1_2013_11431;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
